1982391306c1f4d0cbd4e6acd568bc5e67a5c69e
harmlessman <harmlessman17@gmail.com>
2025-04-01 00:52:53 +0900

    feat: sql문 수정 (req_id -> uuid(char))

--- sql/DDL/summary_project.sql
+++ sql/DDL/summary_project.sql
@@ -6,8 +6,8 @@ CREATE TABLE `summary_project` (
   `title` varchar(1000) DEFAULT NULL,
   `libs` varchar(10000) DEFAULT NULL,
   `deploy_info` varchar(1000) DEFAULT NULL,
-  `req_id` bigint NOT NULL,
+  `req_id` char(36) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `summary_project_request_FK` (`req_id`),
-  CONSTRAINT `summary_project_request_FK` FOREIGN KEY (`req_id`) REFERENCES `request` (`id`) ON DELETE CASCADE
+  CONSTRAINT `summary_project_request_FK` FOREIGN KEY (`req_id`) REFERENCES `summary_request` (`req_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ sql/DDL/summary_request.sql
@@ -0,0 +1,18 @@
+-- torch.request definition
+
+--CREATE TABLE `request` (
+--  `id` bigint NOT NULL AUTO_INCREMENT,
+--  `create_at` timestamp NOT NULL,
+--  `status` varchar(100) DEFAULT NULL,
+--  PRIMARY KEY (`id`)
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE summary_request (
+  id bigint NOT NULL AUTO_INCREMENT,
+  req_id char(36) NOT NULL DEFAULT (UUID()),
+  create_at timestamp NOT NULL,
+  status varchar(100) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (req_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
