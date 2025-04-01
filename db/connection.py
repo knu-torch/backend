fc50882913b1215f091add22893bd5114b015745
harmlessman <harmlessman17@gmail.com>
2025-04-01 11:07:46 +0900

    feat: .env 추가

--- db/connection.py
+++ db/connection.py
@@ -3,7 +3,15 @@ from typing import Annotated
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-engine = create_engine('your db url')
+from dotenv import load_dotenv
+import os
+
+# .env 파일 로드
+load_dotenv()
+
+db_url = os.getenv("DB_URL")
+
+engine = create_engine(db_url)
     
 def get_session():
     with Session(engine) as session:
