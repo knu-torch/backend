c2d3da15d2517701e557eb3d9e8e69d587c8f70a
mimmu524 <gsh244400@naver.com>
2025-04-02 09:56:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ app = FastAPI(debug=True, docs_url="/api/docs",
 """
 option이랑 zip_file받으면,
 1. zipfile unzip 후 디렉토리에 저장,
-2. ai모듈 출력 (arg -> dir_path, option dict)
+2. ai모듈 출력 (arg -> zip_path, options dict)
 3. ai모듈 리턴값 받으면 db에 저장
 4. request id 리턴
 
 ## db running, wait, done 등 (ai 처리 시간이 좀 걸려서)
-## option -> front에서 곧 상세 정보 전달 예정 (string으로 주는데, 파이썬에서 json으로 변경해서 사용)
+## options -> front에서 곧 상세 정보 전달 예정 (string으로 주는데, 파이썬에서 json으로 변경해서 사용)
 """
 
 """
