774ef040f29a3515b90ddd7b12ad14920a76bdb3
mimmu524 <gsh244400@naver.com>
2025-04-04 14:29:29 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,9 +1,12 @@
 from model.enums import summary_options
-from ai_module import AI
-
-result = AI(
-    zip_path = str,
-    options = list[summary_options.SummaryOption]
-)
-
-return result
+ 
+ def AI(
+     dir_path: str,
+     option: list[summary_options.SummaryOption]
+ ):
+ 
+     return {
+         'title' : "title",
+         'libs': "libs",
+         'deploy_info': "deploy_info"
+     }
